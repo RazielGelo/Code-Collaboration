0417a11f7214a6d4c7c04e2c23464db7528b978a
emilsangil <sangilemiljan@gmail.com>
2022-09-26 12:40:08 -0700

    added some sass

--- QUIZ_3/practical/src/pages/index.tsx
+++ QUIZ_3/practical/src/pages/index.tsx
@@ -6,7 +6,12 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div>
-
+      <div>
+        <Image src="/moviebanner.png" alt="banner" width="1700" height="800"></Image>
+      </div>
+      <div>
+        <input type="text" placeholder="SEARCH" />
+      </div>
     </div>
   )
 }
